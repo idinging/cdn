4625d9a2dadd8c1a54858276df8f43c9dec172dc
iding <dincunli@foxmail.com>
2025-08-21 17:16:39 +0800

    Update wrp.js

--- wrp.js
+++ wrp.js
@@ -1,17 +1,2 @@
-export default {
-  async fetch(request, env, ctx) {
-    const url = new URL(request.url)
-    const path = url.pathname === "/" ? "/index.html" : url.pathname
-
-    try {
-      // 从 Assets 里读取文件
-      const res = await env.ASSETS.get(path)
-      if (!res) return new Response("Not Found", { status: 404 })
-      return new Response(res.body, {
-        headers: { "Content-Type": res.metadata?.contentType || "text/plain" },
-      })
-    } catch (err) {
-      return new Response("Error", { status: 500 })
-    }
-  },
-}
+// 这里不要写 Response，否则会覆盖 [site] 的自动逻辑
+export default {}
